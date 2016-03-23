11d14cb7be767b64d5cf70dfd34b9e750fa778fc
Thai Phan <thai@outlook.com>
2016-03-24 01:35:27 +1100

    Finish the job

--- Model/Config/Source/Region.php
+++ Model/Config/Source/Region.php
@@ -3,6 +3,13 @@ namespace Arkade\S3\Model\Config\Source;
 
 class Region implements \Magento\Framework\Option\ArrayInterface
 {
+    private $helper;
+
+    public function __construct(\Arkade\S3\Helper\S3 $helper)
+    {
+        $this->helper = $helper;
+    }
+
     /**
      * Return list of available Amazon S3 regions
      *
@@ -10,47 +17,6 @@ class Region implements \Magento\Framework\Option\ArrayInterface
      */
     public function toOptionArray()
     {
-        return [
-            [
-                'value' => 'us-east-1',
-                'label' => 'US East (N. Virginia)'
-            ],
-            [
-                'value' => 'us-west-2',
-                'label' => 'US West (Oregon)'
-            ],
-            [
-                'value' => 'us-west-1',
-                'label' => 'US West (N. California)'
-            ],
-            [
-                'value' => 'eu-west-1',
-                'label' => 'EU (Ireland)'
-            ],
-            [
-                'value' => 'eu-central-1',
-                'label' => 'EU (Frankfurt)'
-            ],
-            [
-                'value' => 'ap-southeast-1',
-                'label' => 'Asia Pacific (Singapore)'
-            ],
-            [
-                'value' => 'ap-northeast-1',
-                'label' => 'Asia Pacific (Tokyo)'
-            ],
-            [
-                'value' => 'ap-southeast-2',
-                'label' => 'Asia Pacific (Sydney)'
-            ],
-            [
-                'value' => 'ap-northeast-2',
-                'label' => 'Asia Pacific (Seoul)'
-            ],
-            [
-                'value' => 'sa-east-1',
-                'label' => 'South America (Sao Paulo)'
-            ]
-        ];
+        return $this->helper->getRegions();
     }
 }
